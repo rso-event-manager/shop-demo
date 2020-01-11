dcc0d9b7fe0071fde333f56fb744ec4aaa7c751b
Vid Ribic <ribvid@gmail.com>
2020-01-11 11:45:52 +0100

    checkout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,9 @@
 
             <v-row class="justify-center">
                 <v-col cols="10" class="d-flex justify-center">
-                    <div v-for="event in events" :key="event._id" class="white px-8 py-4 text-center elevation-5 last-no-mr" style="border-radius: 7px; min-width: 200px">
+                    <div v-for="event in events" :key="event._id"
+                         class="white px-8 py-4 text-center elevation-5 last-no-mr"
+                         style="border-radius: 7px; min-width: 200px">
                         <h2 class="title font-weight-medium">{{ event.name }}</h2>
                         <p class="body-2 mt-1 mb-0 cyan--text text--darken-3">
                             {{ getDateAndTime(event.startDate) }}
@@ -25,15 +27,18 @@
             </v-row>
         </v-container>
 
-        <v-bottom-sheet v-model="sheet">
-            <v-sheet class="text-center" height="200px">
-                <v-btn
-                        class="mt-6"
-                        text
-                        color="red"
-                        @click="sheet = !sheet"
-                >Prekliči nakup</v-btn>
-                <div class="py-3">This is a bottom sheet using the controlled by v-model instead of activator</div>
+        <v-bottom-sheet v-model="sheet" :eager="true">
+            <v-sheet class="secondary text-center d-flex justify-center align-center" height="200px">
+                <div class="py-10 px-10" style="width: 400px;">
+                    <div ref="card" class="white elevation-5 pa-4" style="border-radius: 10px">
+                        <!-- Elements will create input elements here -->
+                    </div>
+
+                    <!-- We'll put the error messages in this element -->
+                    <div id="card-errors" role="alert"></div>
+
+                    <v-btn outlined color="primary" id="submit" class="mt-7 elevation-4">Plačaj vstopnico</v-btn>
+                </div>
             </v-sheet>
         </v-bottom-sheet>
     </div>
@@ -42,6 +47,10 @@
 <script>
 	const axios = require('axios');
 
+	// eslint-disable-next-line no-undef
+	const stripe = Stripe('pk_test_woNnzLqV7KaTrdLQiiebET4P00vMr2y6uN');
+	const elements = stripe.elements();
+
 	export default {
 		name: 'HelloWorld',
 
@@ -49,7 +58,7 @@
 			events: [],
 			sheet: false,
 		}),
-        methods: {
+		methods: {
 			getDateAndTime: function (eventDate) {
 				const date = new Date(eventDate);
 				const hours = date.getHours() < 10 ? `${date.getHours()}0` : `${date.getHours()}`;
@@ -57,8 +66,8 @@
 
 				return `${date.getDate()}. ${date.getMonth() + 1}. ob ${hours}:${minutes}`;
 			}
-        },
-        mounted() {
+		},
+		mounted() {
 			axios
 				.get(`${process.env.VUE_APP_API}/events`)
 				.then(response => {
@@ -66,16 +75,19 @@
 						this.events = response.data
 					}
 				})
+
+			const card = elements.create("card");
+			card.mount(this.$refs.card);
 		},
 	};
 </script>
 
 <style scoped>
-  .last-no-mr {
-    margin-right: 32px;
-  }
+    .last-no-mr {
+        margin-right: 32px;
+    }
 
-  .last-no-mr:last-child {
-    margin-right: 0;
-  }
+    .last-no-mr:last-child {
+        margin-right: 0;
+    }
 </style>
